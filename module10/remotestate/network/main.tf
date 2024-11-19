be41182babe4be6eecae49084dcc91f275ec8dbb
lillgunn <lill.gunn.lyngaas@tietoevry.com>
2024-11-19 16:04:00 +0100

    Rollback on adress prefix

--- module10/remotestate/network/main.tf
+++ module10/remotestate/network/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_subnet" "main" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.network.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 output "subnet_id" {
